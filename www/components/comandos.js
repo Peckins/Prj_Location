fe3d2ed0dee19ff9d884d6be097a86289dff8913
Chop97 <gustavo.isaque9@gmail.com>
2019-04-03 03:20:30 +0900

    tentei fazer a logica de comandos mas não funciona ainda

---
+++ www/components/comandos.js
@@ -0,0 +1,33 @@
+// This is a JavaScript file
+
+ $(document).on("click","#ir",function(){
+   L.mapquest.key = 'KEY';
+var baseLayer = L.mapquest.tileLayer('dark');
+
+L.mapquest.geocoding().geocode(['New York, NY'], showMap);
+
+function showMap(err, data) {
+  var map = createMap();
+  map.addControl(L.mapquest.control());
+  addLayerControl(map);
+}
+
+function createMap() {
+  var map = L.mapquest.map('map', {
+    center: [40.7237, -73.9825],
+    zoom: 14,
+    layers: baseLayer
+  });
+  return map;
+}
+
+function addLayerControl(map) {
+  L.control.layers({
+    'Map': L.mapquest.tileLayer('map'),
+    'Satellite': L.mapquest.tileLayer('satellite'),
+    'Hybrid': L.mapquest.tileLayer('hybrid'),
+    'Light': L.mapquest.tileLayer('light'),
+    'Dark': baseLayer
+  }, {}, { position: 'topleft'}).addTo(map);
+}
+ });
